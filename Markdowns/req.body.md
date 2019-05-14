9030d3f8c880537175b97ab0ef3232fae36163b8
Nadar <nadaralp16@gmail.com>
2019-05-14 12:51:14 +0300

    integrated express-validator and made request and body validations

---
+++ Markdowns/req.body.md
@@ -0,0 +1,5 @@
+# request.body
+
+#explanation :
+
+- req.body is the object with the data that is sent to the route, now to parse this we have to initialize the body-parser middleware.
--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const connectDB = require("./config/db");
 const app = express();
 // Connecting DB;
 connectDB();
-const PORT = process.env.PORT || 5000;
+
+//Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => res.send("Server is running"));
 
@@ -14,6 +16,7 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
