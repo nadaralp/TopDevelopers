af27cf303ac736b8538bc8d5dd0ed5973233cd1a
Nadar <nadaralp16@gmail.com>
2019-05-15 16:24:53 +0300

    Fixed a Post Model bug and created POST route for post creation

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
 const PostSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
@@ -26,7 +27,7 @@ const PostSchema = new Schema({
   comments: [
     {
       user: {
-        types: Schema.Types.ObjectId,
+        type: Schema.Types.ObjectId,
         ref: 'users'
       },
       text: {
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,46 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+const auth = require('../../middlewares/auth');
+const User = require('../../models/User');
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 
-// @route   GET api/posts
-// @desc    Test route
-// @access  Public
-router.get("/", (req, res) => res.send("posts router"));
+// @route   Post api/posts
+// @desc    Create a post
+// @access  Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) return res.json({ errors: errors.array() });
+
+      const user = await User.findById(req.user.id).select('-password');
+
+      const newPost = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      };
+
+      const post = new Post(newPost);
+      await post.save();
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
