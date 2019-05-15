fce7c873407ff2bb8db9d32e88cc0e6b653c3cfc
Nadar <nadaralp16@gmail.com>
2019-05-15 13:15:11 +0300

    delete route for profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -138,4 +138,21 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+// @route   DELETE api/profile
+// @desc    Delete profile, user and post
+// @access  Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    // remove Profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+    // Remove post
+    res.json({ msg: "User deleted" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
