54c098ce28343ff15467133ef31342bdea5d9f09
Nadar <nadaralp16@gmail.com>
2019-06-05 15:34:52 +0300

    Display profiles with Porfile component and profile item component

--- client/src/components/layout/NavBar.js
+++ client/src/components/layout/NavBar.js
@@ -8,6 +8,7 @@ const NavBar = ({ auth: { isAuthenticated, loading }, logout }) => {
 
     const authLinks = (
         <ul>
+            <li><Link to="/profiles"><i className="fas fa-users"></i><span className="hide-sm">{' '}Developers</span></Link></li>
             <li><Link to="/dashboard"><i className="fas fa-user"></i><span className="hide-sm">{' '}Dashboard</span></Link></li>
             <li><Link onClick={logout} to="#"><i className="fas fa-sign-out-alt"></i>{'  '}<span className="hide-sm">Logout</span></Link></li>
         </ul>
@@ -15,7 +16,7 @@ const NavBar = ({ auth: { isAuthenticated, loading }, logout }) => {
 
     const guestLinks = (
         <ul>
-            <li><Link to="/profiles">Developers</Link></li>
+            <li><Link to="/profiles"><i className="fas fa-users"></i><span className="hide-sm">{' '}Developers</span></Link></li>
             <li><Link to="/register">Register</Link></li>
             <li><Link to="/login">Login</Link></li>
         </ul>
---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,44 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import { getAllProfiles } from '../../actions/profile';
+import { AUTH_ERROR } from '../../actions/types';
+import ProfileItem from './ProfileItem';
+
+const Profiles = ({ getAllProfiles, profile: { profiles, loading } }) => {
+    useEffect(() => {
+        getAllProfiles();
+    }, [])
+    return (
+        <Fragment>
+            {
+                loading ? <Spinner /> : <Fragment>
+                    <h1 className="large text-primary">Developers</h1>
+                    <p className="lead">
+                        <i className="fab fa-connectdevelop"></i>{'  '}
+                        Browse and connect with developers
+                    </p>
+                    <div className="profiles">
+                        {profiles.length > 0 ? profiles.map((profile) => (
+                            <ProfileItem key={profile._id} profile={profile} />
+                        )) : <h4>No Profiles Found</h4>}
+                    </div>
+                </Fragment>
+            }
+        </Fragment>
+    )
+}
+
+Profiles.propTypes = {
+    getAllProfiles: PropTypes.func.isRequired,
+    profile: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    profile: state.profile
+});
+
+export default connect(mapStateToProps, {
+    getAllProfiles
+})(Profiles);
