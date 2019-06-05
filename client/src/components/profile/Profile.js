5075c53846dfc42191a616105eba758be2f9ed9f
Nadar <nadaralp16@gmail.com>
2019-06-05 22:07:10 +0300

    Profile Experience and Profile Education sub components added

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,8 @@ import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience.js';
+import ProfileEducation from './ProfileEducation.js';
 
 const Profile = ({ getProfileById, match, profile: { profile, loading }, auth }) => {
     useEffect(() => {
@@ -26,6 +28,30 @@ const Profile = ({ getProfileById, match, profile: { profile, loading }, auth })
                     <div className="profile-grid my-1">
                         <ProfileTop profile={profile} />
                         <ProfileAbout profile={profile} />
+                        <div className="profile-exp bg-white p-2">
+                            <h2 className="text-primary">Experience</h2>
+                            {
+                                profile.experience.length > 0 ?
+                                    <Fragment>
+                                        {
+                                            profile.experience.map((exp) => <ProfileExperience key={exp._id} experience={exp} />)
+                                        }
+                                    </Fragment>
+                                    : <h4>No Experience Credentials</h4>
+                            }
+                        </div>
+                        <div className="profile-edu bg-white p-2">
+                            <h2 className="text-primary">Education</h2>
+                            {
+                                profile.education.length > 0 ?
+                                    <Fragment>
+                                        {
+                                            profile.education.map((edu) => <ProfileEducation key={edu._id} education={edu} />)
+                                        }
+                                    </Fragment>
+                                    : <h4>No Education Credentials</h4>
+                            }
+                        </div>
                     </div>
                 </Fragment>
             }
