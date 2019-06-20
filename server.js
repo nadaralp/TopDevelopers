54172141a12c2320ce75d059cfc21cd1bb36dbfb
Nadar <nadaralp16@gmail.com>
2019-06-20 22:40:33 +0300

    fixed a bug with serving the files and server error

--- server.js
+++ server.js
@@ -29,11 +29,13 @@ app.use("/api/posts", require("./routes/api/posts"));
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
+
+
+  app.get('*', (req, res) => {
+    res.sendFiles(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
 
-app.get('*', (req, res) => {
-  res.sendFiles(path.resolve(__dirname, 'client', 'build', 'index.html'))
-})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
